75aeb90918c97ed5ec05dbf7d82c651acf75c5d4
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-06-08 18:02:10 +0200

    refactor: 💡 Switch to console log

--- src/main.ts
+++ src/main.ts
@@ -68,7 +68,7 @@ async function run(): Promise<void> {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(JSON.stringify(await getCoverage()))
+    console.log('Coverage', JSON.stringify(await getCoverage()))
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
