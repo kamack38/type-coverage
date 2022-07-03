2e5e7167183b7ea9c6ec6a32c6f1947111ccd601
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-07-03 21:39:27 +0200

    feat: 🎸 Debug input

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,29 @@ import generateCoverageReport, { getCoverage, ProgramOptions } from './coverage'
 
 async function run(): Promise<void> {
   try {
-    const tsConfigPath: string = core.getInput('tsConfigPath')
+    const tsConfigPath: string = core.getInput('tsConfigPath') ?? '.'
     const threshold: number = parseInt(core.getInput('threshold'))
     const strict: boolean = core.getInput('strict') === 'true' ? true : false
     const ignoreCatch: boolean =
       core.getInput('ignoreCatch') === 'true' ? true : false
     const ignoreUnread: boolean =
       core.getInput('ignoreUnread') === 'true' ? true : false
-    const debug: boolean = core.getInput('debug') === 'true' ? true : false
     const ignoreFiles: string[] = core.getInput('ignoreFiles').split('\n')
+    const debug: boolean = core.getInput('debug') === 'true' ? true : false
     console.log(
       'Coverage',
       JSON.stringify(
         await getCoverage({ tsProjectFile: tsConfigPath, strict, debug })
       )
+    )
+    console.log(
+      tsConfigPath,
+      threshold,
+      strict,
+      ignoreCatch,
+      ignoreUnread,
+      ignoreFiles,
+      debug
     )
     const options: ProgramOptions = {
       tsProjectFile: tsConfigPath,
